b0d8a1752e0cf37b960a80414ebef9dc276715a8
palvin254 <palvinpaul900m@gmail.com>
2022-07-05 14:07:54 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,49 @@
 #include "main.h"
-
 /**
- *   main - check the code.
- *   *
- *     Return: Always 0.
+ *  * print_times_table - entry point
+ *   * @size: the size of the multplication table
+ *    * Return: void
 */
-int main(void)
+void print_times_table(int size)
 {
-times_table(3);
+if ((size >= 0) && (size <= 15))
+{
+int row, col = size, res;
+
+for (row = 0; row <= size; row++)
+{
+for (col = 0; col <= size; col++)
+{
+res = row * col;
+if ((res == 0) && (col == 0))
+_putchar(res + '0');
+else if (res <= 9)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(res + '0');
+}
+else if ((res >= 10) && (res < 100))
+{
+_putchar(' ');
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else
+{
+_putchar((res / 100) + '0');
+_putchar((res % 100 / 10) + '0');
+_putchar((res % 10) + '0');
+}
+if (col != size)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
 _putchar('\n');
-times_table(5);
-_putchar('\n');
-times_table(98);
-_putchar('\n');
-times_table(12);
-return (0);
+}
+}
+else
+return;
 }
