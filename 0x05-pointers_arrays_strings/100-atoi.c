d467d5678c56f13a852cb4b7c406b9540e56ab48
palvin254 <palvinpaul900m@gmail.com>
2022-07-11 04:25:01 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *_atoi - convert string to int
+ *@s: pointer to string
+ *Return: int
+*/
+int _atoi(char *s)
+{
+unsigned int number, i;
+int sign;
+
+sign = 1;
+number = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (is_numerical(s[i]))
+{
+number = (s[i] - 48) + number * 10;
+
+if (s[i + 1] == ' ')
+break;
+}
+
+else if (s[i] == '-')
+{
+sign *= -1;
+}
+}
+
+return (number * sign);
+}
